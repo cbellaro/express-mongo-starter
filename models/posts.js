899638d6e165934b78dd448a7bf097bdb733395b
Chris Bellaro <chrisbellaro@Chriss-Air.home>
2020-03-02 21:39:25 -0500

    added author to post schema, fixed page styling accordingly

--- models/posts.js
+++ models/posts.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
     title: {type: String, required: true},
+    author: {type: String, required: true},
     chapters: {type: String, required: true},
     body: String
 })
